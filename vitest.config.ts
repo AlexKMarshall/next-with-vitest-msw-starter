0a6c9f6c0b43dec77e58235b15ad2f283647a1dc
AlexKMarshall <alex.k.marshall83@gmail.com>
2022-08-07 08:31:47 +0100

    install happy-dom

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,14 @@
 
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
   test: {
-    environment: "jsdom",
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: "src/test/config.ts",
   },
 });
